219d06108719cdc0b8356dd9446e8a05c5c2d11b
Denis Suvorov <69597295+Erkobrax@users.noreply.github.com>
2023-11-20 19:33:07 +0300

    update

--- invite.js
+++ invite.js
@@ -5,4 +5,26 @@ const repoOwner = 'Erkobrax'; // Имя пользователя, владель
 const repoName = 'invitescript'; // Название репозитория
 const filePath = 'usernames.json'; // Путь к JSON файлу в репозитории
 
-axios.get('')
+axios.get('https://api.github.com/repos/Erkobrax/invitescript/contents/username.json',{
+    headers:{
+        'Authorization': 'token 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',
+        'Accept': 'application/vnd.github.v3.raw'
+}
+})
+.then(response =>{
+    const users = JSON.parse(response.data);
+    inviteUsers(users); // добавить инвайт
+});
+
+function inviteUsers(users) {
+    users.forEach(user => {
+        if (user.github){
+            axios.post('https://api.github.com/orgs/Suvorov-Kamyshnikov/invitations', {
+                invitee_id: user.github
+            }, {
+                headers: {'Authorization': 'token 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'}
+                })
+                .then(()=> console.log(`Приглашение отправлено пользователю ${user.github}`))
+        }
+    });
+}
