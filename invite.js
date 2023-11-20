571dec0c102adff82451b92ad5b41dbd22fdf07c
Denis Suvorov <69597295+Erkobrax@users.noreply.github.com>
2023-11-20 19:19:10 +0300

    update

---
+++ invite.js
@@ -0,0 +1,8 @@
+const axios = require('axios');
+const token = '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'; // Ваш GitHub Personal Access Token
+const org = 'Suvorov-Kamyshnikov'; // Название вашей организации на GitHub
+const repoOwner = 'Erkobrax'; // Имя пользователя, владельца репозитория
+const repoName = 'invitescript'; // Название репозитория
+const filePath = 'usernames.json'; // Путь к JSON файлу в репозитории
+
+axios.get('')
